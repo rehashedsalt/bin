422d0f29941037eed67093f72c3cdd52e1edded0
Salt <rehashedsalt@cock.li>
2018-07-14 14:35:26 -0500

    replicate.sh: Make POSIX, hide untracked files

--- bootstrap/replicate.sh
+++ bootstrap/replicate.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
-tempfolder="/tmp/dots$(uuidgen)"
-git clone --recursive --separate-git-dir="$HOME/.dotfiles" https://github.com/rehashedsalt/home.git $tempfolder
+#!/bin/sh
+tempfolder="/tmp/dots`uuidgen`"
+gitdir="$HOME/.dotfiles"
+git clone --recursive --separate-git-dir="$gitdir" https://github.com/rehashedsalt/home.git $tempfolder
 rsync -rvl --exclude ".git" $tempfolder/ $HOME/
 rm -r $tempfolder
-git submodule update --init --recursive --remote --git-dir="$HOME/.dotfiles" --work-tree="$HOME" $HOME/
+git submodule update --init --recursive --remote --git-dir="$gitdir" --work-tree="$HOME" $HOME/
+git config --git-dir="$gitdir" status.showUntrackedFiles no
