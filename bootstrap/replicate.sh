60f54f52015b94d28d4a545bbdc9a26929cf93e0
Salt <rehashedsalt@cock.li>
2018-07-14 14:59:08 -0500

    replicate.sh: Use SSH again

--- bootstrap/replicate.sh
+++ bootstrap/replicate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 tempfolder="/tmp/dots`uuidgen`"
 gitdir="$HOME/.dotfiles"
-git clone --recursive --separate-git-dir="$gitdir" https://github.com/rehashedsalt/home.git $tempfolder
+git clone --recursive --separate-git-dir="$gitdir" git@github.com:rehashedsalt/home.git $tempfolder
 rsync -rvl --exclude ".git" $tempfolder/ $HOME/
 rm -r $tempfolder
 git submodule update --init --recursive --remote --git-dir="$gitdir" --work-tree="$HOME" $HOME/
